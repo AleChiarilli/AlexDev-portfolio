00b7d95d918bfb07f03fa91cdc0e23d167bb45d0
AleChiarilli <alejandrochiarilli@gmail.com>
2023-11-13 19:31:40 +0100

    imports for particles

--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -1,7 +1,9 @@
-
+"use client";
+import { useCallback } from "react";
+import { Particles } from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import type { Engine } from "tsparticles-engine";
 
 export function Cover() {
-    return (
-        <div>Cover</div>
-    )
+  return <div>Cover</div>;
 }
