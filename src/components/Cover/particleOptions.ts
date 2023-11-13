f9ca346eb583e1bfdfed13a6f8ffcfd23fdfda0d
AleChiarilli <alejandrochiarilli@gmail.com>
2023-11-13 20:07:45 +0100

    feat: background react-particles

--- src/components/Cover/particleOptions.ts
+++ src/components/Cover/particleOptions.ts
@@ -17,8 +17,8 @@ export const optionsParticles = {
         quantity: 4,
       },
       repulse: {
-        distance: 200,
-        duration: 0.4,
+        distance: 80,
+        duration: 8.4,
       },
     },
   },
@@ -54,7 +54,7 @@ export const optionsParticles = {
       value: 0.5,
     },
     shape: {
-      type: "circle",
+      type: "triangle",
     },
     size: {
       value: { min: 1, max: 5 },
